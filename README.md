8441b122140c1e8f1d724ea2a67207ede9d31d41
Moaid Hashem <Gamestorrent2015@gmail.com>
2025-10-10 23:00:38 +0300

    - Setting up

---
+++ README.md
@@ -0,0 +1,5 @@
+## Command Line Interpreter - Assignment 1 - OS
+
+a simple command line interpreter that takes in a linux command and arguments and executes it.
+
+
---
+++ src/Terminal.java
@@ -0,0 +1,13 @@
+class Parser{
+
+    String commandName;
+    String[] args;
+    
+}
+public class Terminal{
+    Parser parser;
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
