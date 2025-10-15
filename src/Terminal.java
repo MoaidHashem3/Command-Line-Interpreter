2a4b444dda0c48a8ee142c55754eca8001b2ad77
Moaid Hashem <Gamestorrent2015@gmail.com>
2025-10-15 14:12:44 +0300

    Added zip and unzip commands

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,5 +1,8 @@
 import java.io.*;
 import java.util.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class Terminal {
     public static void main(String[] args) {
@@ -89,7 +92,6 @@ public class Terminal {
         }
     }
 
-
     void pwd() {
         System.out.println("Nada's part");
     }
@@ -131,12 +133,144 @@ public class Terminal {
     }
 
     void zip(String[] args) {
-        System.out.println("Moaid's part");
+
+        if (args.length < 2) {
+            System.out.println("Wrong Input please use: zip <zip_file_name.zip> <file1> <file2> ...");
+            return;
+        }
+
+        String zipFile;
+        int startIndex = 0;
+        boolean recursive = false;
+
+        if (args[0].equals("-r")) {
+
+            if (args.length < 3) {
+                System.out.println("Wrong Input please use: zip -r <archive.zip> <directory>");
+                return;
+            }
+            recursive = true;
+            zipFile = args[1];
+            startIndex = 2;
+        } else {
+            zipFile = args[0];
+            startIndex = 1;
+        }
+
+        try (FileOutputStream fos = new FileOutputStream(zipFile);
+                ZipOutputStream zos = new ZipOutputStream(fos)) {
+
+            if (recursive) {
+                File dir = new File(args[startIndex]);
+                if (!dir.exists()) {
+                    System.out.println("Directory not found: " + args[startIndex]);
+                    return;
+                }
+                addDirectoryToZip(dir, dir.getName(), zos);
+                System.out.println("Directory compressed: " + dir.getPath());
+            } else {
+
+                for (int i = 1; i < args.length; i++) {
+                    File fileToZip = new File(args[i]);
+
+                    if (!fileToZip.exists()) {
+                        System.out.println(args[i] + " (file not found)");
+                        continue;
+                    }
+
+                    addFileToZip(fileToZip, fileToZip.getName(), zos);
+
+                }
+            }
+            System.out.println("Commpressing is complete and Zip file created: " + zipFile);
+
+        } catch (IOException e) {
+            System.out.println("Error zipping: " + e.getMessage());
+        }
+    }
+
+    private void addFileToZip(File fileToZip, String fileName, ZipOutputStream zos) throws IOException {
+        if (fileToZip.isDirectory()) {
+            addDirectoryToZip(fileToZip, fileName, zos);
+            return;
+        }
+        try (FileInputStream fis = new FileInputStream(fileToZip)) {
+            zos.putNextEntry(new ZipEntry(fileName));
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = fis.read(buffer)) >= 0) {
+                zos.write(buffer, 0, length);
+            }
+            zos.closeEntry();
+        }
+    }
+
+    private void addDirectoryToZip(File folder, String parentFolder, ZipOutputStream zos) throws IOException {
+        File[] files = folder.listFiles();
+        if (files == null || files.length == 0) {
+
+            zos.putNextEntry(new ZipEntry(parentFolder + "/"));
+            zos.closeEntry();
+            return;
+        }
+
+        for (File file : files) {
+            if (file.isDirectory()) {
+                addDirectoryToZip(file, parentFolder + "/" + file.getName(), zos);
+            } else {
+                addFileToZip(file, parentFolder + "/" + file.getName(), zos);
+            }
+        }
     }
 
     void unzip(String[] args) {
-        System.out.println("Moaid's part");
+        if (args.length < 1) {
+            System.out.println("Wrong Input please use:");
+            System.out.println("  unzip <archive.zip>");
+            System.out.println("  unzip <archive.zip> -d <destination_folder>");
+            return;
+        }
+
+        String zipFile = args[0];
+        String destDir = ".";
+
+        if (args.length >= 3 && args[1].equals("-d")) {
+            destDir = args[2];
+        }
+
+        File dir = new File(destDir);
+        if (!dir.exists())
+            dir.mkdirs();
+
+        byte[] buffer = new byte[1024];
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile))) {
+            ZipEntry zipEntry = zis.getNextEntry();
+            while (zipEntry != null) {
+                File newFile = new File(dir, zipEntry.getName());
+
+                if (zipEntry.isDirectory()) {
+                    newFile.mkdirs();
+                } else {
+                    new File(newFile.getParent()).mkdirs();
+                    try (FileOutputStream fos = new FileOutputStream(newFile)) {
+                        int len;
+                        while ((len = zis.read(buffer)) > 0) {
+                            fos.write(buffer, 0, len);
+                        }
+                    }
+                    System.out.println("Zip extracted: " + newFile.getPath());
+                }
+
+                zipEntry = zis.getNextEntry();
+            }
+            zis.closeEntry();
+            System.out.println("Unzip is completed: " + dir.getAbsolutePath());
+
+        } catch (IOException e) {
+            System.out.println("Error while unzipping: " + e.getMessage());
+        }
     }
+
 }
 
 class Parser {
@@ -182,6 +316,7 @@ class Parser {
     public String getCommandName() {
         return commandName;
     }
+
     public String[] getArgs() {
         return args;
     }
